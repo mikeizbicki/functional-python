29b1c2a164de8ea85d95afb007689a7808561bc7
Katie Chen <katherine.chen.23@cmc.edu>
2021-02-03 20:31:37 -0800

    ran linter

--- Fixme.py
+++ Fixme.py
@@ -2,8 +2,10 @@
 '''
 Your solution for each function should be only a single line long.
 In particular, you may not use any loops when implementing these functions;
-instead, you must use either the map and filter functions or list comprehensions.
+instead, you must use either the map and filter functions or
+list comprehensions.
 '''
+
 
 def evens(n):
     '''
@@ -20,20 +22,18 @@ def evens(n):
     >>> evens(-1)
     []
     '''
-    #xs = []
-    #for i in range(n+1):
-    #    if i%2==0:
-    #        xs.append(i)
-    #return xs
-    
-    return list(filter(lambda x: x%2==0, range(n+1)))
-
-
+    # xs = []
+    # for i in range(n+1):
+    #     if i%2==0:
+    #         xs.append(i)
+    # return xs
+    return list(filter(lambda x: x % 2 == 0, range(n+1)))
 
 
 def threes(n):
     '''
-    Returns a list of all numbers from 0 to n inclusive that contain the digit 3.
+    Returns a list of all numbers from 0 to n
+    inclusive that contain the digit 3.
 
     >>> threes(2)
     []
@@ -46,20 +46,22 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
-    #xs = []
-    #for i in range(n+1):
-    #    if i ==3 or i%10==3 or i//10==3: 
-    #        xs.append(i)
-    #return xs
+    # xs = []
+    # for i in range(n+1):
+    #     if i ==3 or i%10==3 or i//10==3:
+    #         xs.append(i)
+    # return xs
 
-    return [ i for i in range(n+1) if i == 3 or i%10 == 3 or i//10 == 3 ]
-    
-    #list(filter(lambda x: i=3, i%10==3, i//10==3, range(n+1)))
-    #return threes
+    return [i for i in range(n+1) if i == 3 or i % 10 == 3 or i//10 == 3]
+
+    # list(filter(lambda x: i=3, i%10==3, i//10==3, range(n+1)))
+    # return threes
+
 
 def small_words(text):
     '''
-    Returns a list of all words in the input text that are less than 4 characters long.
+    Returns a list of all words in the input text that are
+    less than 4 characters long.
 
     HINT:
     Recall that text.split() converts the text variable into a list of words.
@@ -73,7 +75,7 @@ def small_words(text):
     >>> small_words('a big word is bad')
     ['a', 'big', 'word', 'is', 'bad']
     '''
-    return [ word for word in text.split() if len(word)<=4 ]
+    return [word for word in text.split() if len(word) <= 4]
 
 
 def squares(n):
@@ -90,12 +92,13 @@ def squares(n):
     >>> squares(10)
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
-    return [ i*i for i in range(1,n+1) ]
+    return [i*i for i in range(1, n+1)]
 
-         
+
 def lengths(strings):
     '''
-    Given a list of strings, returns a list of the lengths of the corresponding strings.
+    Given a list of strings, returns a list of the lengths of the
+    corresponding strings.
 
     >>> lengths([])
     []
@@ -104,10 +107,9 @@ def lengths(strings):
     >>> lengths(['this','is','a','test'])
     [4, 2, 1, 4]
     '''
-    #xs = []
-    #for i in strings:
-    #    xs.append(len(i))
-    #return xs
+    # xs = []
+    # for i in strings:
+    #     xs.append(len(i))
+    # return xs
 
     return [len(word) for word in strings]
-
